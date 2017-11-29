fef652ac6f36839e12bbd45bb7c2d1203d901932
Andrew Couture <couture.andrew@gmail.com>
2017-11-29 21:04:57 +0000

    adds webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: "/src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: [ 'env', 'react' ]
+        }
+      },
+      {
+        test: /.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /.scss$/,
+        loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      }
+    ]
+  },
+  devtool: "source-map"
+};
