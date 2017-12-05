2f0bf2ffaa70d27191bebe8f62f890a7124435f4
Andrew Couture <couture.andrew@gmail.com>
2017-12-05 21:45:17 +0000

    test but no coverage

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,28 +1,137 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const url = require('url');
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
 
-let win;
+module.exports = require("electron");
 
-const createWindow = () => {
-  win = new BrowserWindow({ width: 800, height: 600 });
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _electron = __webpack_require__(0);
+
+var _createWindow = __webpack_require__(2);
+
+_electron.app.on('ready', _createWindow.createWindow);
+
+_electron.app.on('activate', function () {
+  win === null && (0, _createWindow.createWindow)();
+});
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _require = __webpack_require__(0),
+    BrowserWindow = _require.BrowserWindow;
+
+var path = __webpack_require__(3);
+var url = __webpack_require__(4);
+
+var createWindow = exports.createWindow = function createWindow(windowConfig) {
+  var win = new BrowserWindow(windowConfig);
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file',
     slashes: true
   }));
 
-  win.webContents.openDevTools();
-  win.on('closed', () => {
+  win.on('show', function () {
+    win.webContents.openDevTools();
+  });
+
+  win.on('closed', function () {
 
     win = null;
   });
-
 };
 
-app.on('ready', createWindow);
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
 
-app.on('activate', () => {
-  win === null && createWindow();
-});
+module.exports = require("path");
 
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+module.exports = require("url");
+
+/***/ })
+/******/ ]);
